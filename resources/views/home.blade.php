1e489090dcf8d95104b76a5a8452425d23f32743
Rubel Mahmud <vxlrubel@gmail.com>
2025-02-18 01:27:28 +0600

    create home page for initial view.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@push('title', 'Fresh & Delicious Food Delivered to Your Doorstep')
+
+@push('metacontent')
+
+<meta name="description" content="FoodCart is your one-stop online food marketplace, offering fresh and delicious meals delivered straight to your home. Browse our menu and order now!">
+<meta name="keywords" content="food delivery, online food shopping, fresh meals, restaurant delivery, FoodCart">
+<meta name="author" content="FoodCart Team">
+
+@endpush
+
+@section('content')
+
+<section class="clearfix">
+    <div class="container">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam distinctio culpa animi corrupti. Ex veritatis, ut, fuga vero similique, cum provident magni officia impedit nemo architecto quasi reprehenderit nam enim. <i class="fa-sharp-duotone fa-solid fa-bars"></i>
+    </div>
+</section>
+
+
+@endsection
