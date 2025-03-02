eee3132a2274cf0623c4995e274b64c19549d56b
Rubel Mahmud <vxlrubel@gmail.com>
2025-03-02 23:46:50 +0600

    create subscribe form for footer section.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,19 @@
 <footer class="footer-area">
+
+    <div class="subscribe-us bg-white">
+        <div class="container">
+            <form action="javascript:void(0)" class="fc-subscribe-form">
+                <div class="input-group border">
+                    <span class="input-group-text rounded-0 border-0">@</span>
+                    <input type="email" class="form-control rounded-0 shadow-none border-0 small py-2" placeholder="Email Address">
+                    <button class="input-group-text rounded-0 border-0" type="submit">
+                        <i class="fa-light fa-paper-plane"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
     <div class="container">
         &copy;Copyright All right reserved by <a href="javascript:void(0)">Rubel Mahmud</a>
     </div>
