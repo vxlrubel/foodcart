543abed71f9ae9572564d56b02d6329ad609dc18
Rubel Mahmud <vxlrubel@gmail.com>
2025-03-03 22:00:45 +0600

    update footer section .

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -14,38 +14,72 @@
         </div>
     </div>
 
-    <div class="py-4">
+    <div class="py-4 footer-content">
         <div class="container">
             <div class="row" style="--bs-gutter-y: 1.225rem;">
                 <div class="col-sm-6 col-lg-3">
                     <h2 class="fs-5 text-uppercase m-0 py-1 border-bottom border-primary text-primary mb-2">Find us</h2>
                     <div>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem mollitia voluptatem reprehenderit quis consequuntur architecto.
+                        <div class="mb-3"><i class="fa-light fa-envelope"></i> sandrafoodsltd@gmail.com</div>
+                        <div class="d-flex align-items-center gap-2 social-icons">
+                            <a href="javascript:void(0)" target="_blank">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                            <a href="javascript:void(0)" target="_blank">
+                                <i class="fa-brands fa-youtube"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <div class="col-sm-6 col-lg-3">
                     <h2 class="fs-5 text-uppercase m-0 py-1 border-bottom border-primary text-primary mb-2">Information</h2>
-                    <div>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem mollitia voluptatem reprehenderit quis consequuntur architecto.
-                    </div>
+                    <ul class="list-unstyled m-0 p-0 footer-list">
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Shipping & returns</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Privacy notice</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Conditions of Use</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">About us</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Contact us</a></li>
+                    </ul>
                 </div>
                 <div class="col-sm-6 col-lg-3">
                     <h2 class="fs-5 text-uppercase m-0 py-1 border-bottom border-primary text-primary mb-2">Customer service</h2>
-                    <div>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem mollitia voluptatem reprehenderit quis consequuntur architecto.
-                    </div>
+                    <ul class="list-unstyled m-0 p-0 footer-list">
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Search</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Blog</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Recently viewed products</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Compare products list</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">New products</a></li>
+                    </ul>
                 </div>
                 <div class="col-sm-6 col-lg-3">
                     <h2 class="fs-5 text-uppercase m-0 py-1 border-bottom border-primary text-primary mb-2">My account</h2>
-                    <div>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem mollitia voluptatem reprehenderit quis consequuntur architecto.
-                    </div>
+                    <ul class="list-unstyled m-0 p-0 footer-list">
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">My account</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Orders</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Addresses</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Shopping cart</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Wishlist</a></li>
+                        <li><a href="javascript:void(0)" class="d-inline-block text-decoration-none text-dark py-1">Apply for vendor account</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="container">
-        &copy;Copyright All right reserved by <a href="javascript:void(0)">Rubel Mahmud</a>
+    <div class="py-3 copyright">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="text-center text-md-start">&copy;Copyright <span id="year"></span>. All right resevered.</div>
+                </div>
+                <div class="col-md-6">
+                    <div class="text-center text-md-end">Powered by <a href="javascript:void(0)" class="text-decoration-none text-primary fw-bold">Techspace</a></div>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
+
+<script>
+    document.getElementById("year").textContent = new Date().getFullYear();
+</script>
