9a401816a763e0e73f937d88b8df87d31d180a1f
Rubel Mahmud <vxlrubel@gmail.com>
2025-03-05 00:43:18 +0600

    update the configuration of hero slider.

--- public/assets/js/pages/home.js
+++ public/assets/js/pages/home.js
@@ -5,10 +5,43 @@ const App = Vue.createApp({
         };
     },
     methods: {
-        heroSlider(){
-            const swiper = new Swiper('.swiper', {
-                speed: 400,
+        heroSlider() {
+            const progressCircle = document.querySelector(".autoplay-progress svg");
+            const progressContent = document.querySelector(".autoplay-progress span");
+
+            const swiper = new Swiper('.hero-slider', {
+                speed: 1000,
                 spaceBetween: 100,
+                autoplay: {
+                    delay: 5000,
+                    disableOnInteraction: false,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true
+                },
+                on: {
+                    autoplayTimeLeft(s, time, progress) {
+                      progressCircle.style.setProperty("--progress", 1 - progress);
+                      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+                    }
+                },
+                parallax: true,
+                grabCursor: true,
+                effect: "creative",
+                creativeEffect: {
+                  prev: {
+                    shadow: true,
+                    translate: ["-20%", 0, -1],
+                  },
+                  next: {
+                    translate: ["100%", 0, 0],
+                  },
+                },
             });
         }
     },
