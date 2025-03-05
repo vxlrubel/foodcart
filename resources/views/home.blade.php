6477b9671f0eda1dde159cfec7e16e649b13604a
Rubel Mahmud <vxlrubel@gmail.com>
2025-03-05 23:41:19 +0600

    update home page for new products section.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,7 @@
 
 @section('content')
 
+{{-- hero slider --}}
 <section class="clearfix">
     <div class="container-fluid p-0">
         <div class="swiper hero-slider">
@@ -77,6 +78,54 @@
         </div>
     </div>
 </section>
+{{-- hero slider end --}}
+
+{{-- new product start --}}
+<section class="clearfix py-4 py-lg-5">
+    <div class="container">
+        <div class="header-title">
+            <h4 class="fs-4 text-uppercase">New Products</h4>
+            <div>@{{products.length}} items</div>
+        </div>
+        <div class="swiper new-products">
+            <div class="swiper-wrapper mb-5">
+                <div class="swiper-slide new-product-item border" v-for="(product, index) in products" :key="product.id">
+                    <div class="new-product-images">
+                        <div class="status">@{{product.status}}</div>
+                        <img :src="'assets/img/new-products/' + product.image" alt="" class="img-fluid" v-if="product.image">
+                        <img src="{{asset('/assets/img/new-products/placeholder.jpg')}}" alt="" class="img-fluid" v-else>
+                        <div class="action">
+                            <button class="btn btn-sm btn-primary rounded-1">
+                                <i class="fa-regular fa-heart"></i>
+                            </button>
+                            <button class="btn btn-sm btn-primary rounded-1">
+                                <i class="fa-regular fa-arrow-right-arrow-left"></i>
+                            </button>
+                            <button class="btn btn-sm btn-primary rounded-1">
+                                <i class="fa-regular fa-eye"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="p-3 text-center text-primary">
+                        <div class="text-capitalize text-truncate">@{{ product.name }}</div>
+                        <div class="small fw-bold mb-1"><span>৳</span>@{{ parseFloat(product.price).toFixed(2) }}</div>
+                        <div class="d-flex justify-content-center align-items-center gap-1 small mb-2">
+                            <i class="fa-light fa-star"></i>
+                            <i class="fa-light fa-star"></i>
+                            <i class="fa-light fa-star"></i>
+                            <i class="fa-light fa-star"></i>
+                            <i class="fa-light fa-star"></i>
+                        </div>
+                        <button class="btn btn-sm btn-primary rounded-1 py-2 px-3 text-uppercase"><i class="fa-regular fa-cart-plus"></i> Add To cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+    </div>
+</section>
+{{-- new product end --}}
+
 
 
 @endsection
